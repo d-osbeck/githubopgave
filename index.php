9fae9ba181f1c415d2084f3c4f2eecb39fc9e99b
David <david.osbeck1996@gmail.com>
2021-03-03 10:43:29 +0100

    git init

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!-- saved from url=(0086)file:///C:/Users/david/AppData/Local/Temp/Rar.22826/HTML-grundopsaetning.html -->
+<html lang="da"><!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+
+
+    <!-- Titel som ses oppe i browserens tab mv. -->
+    <title>Sigende titel</title>
+
+    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+<body>
+
+<!-- Her skal sidens indhold ligge -->
+
+
+</body>
+</html>
